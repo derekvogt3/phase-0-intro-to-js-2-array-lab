a8a48aaa723b55f2381e810dffdd56ed19f4dd9e
Derek Vogt <derekvogt3@gmail.com>
2022-05-17 12:23:25 -0500

    commit tree 8f90bb1298aa773929a1e2956ac8b5641a763f58

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Write your solution here!
+
+var cats = ["Milo", "Otis", "Garfield"];
+function destructivelyAppendCat(name) {
+  cats.push(name);
+}
+
+function destructivelyPrependCat(name) {
+  cats.unshift(name);
+}
+
+function destructivelyRemoveFirstCat() {
+  cats.shift();
+}
+
+function destructivelyRemoveLastCat() {
+  cats.pop();
+}
+
+function appendCat(name) {
+  return [...cats, name];
+}
+
+function prependCat(name) {
+  return [name, ...cats];
+}
+
+function removeLastCat() {
+  var newCats = [...cats];
+  newCats.pop();
+  print(newCats);
+  return newCats;
+}
+
+function removeFirstCat() {
+  console.log(cats);
+  var newCats = [...cats];
+  newCats.shift();
+  return newCats;
+}
